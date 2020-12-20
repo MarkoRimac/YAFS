aa3a11ab40266f25cb5e4b28f5273a7a7f285e46
Marko Rimac <mrimac97@gmail.com>
2020-12-20 18:17:10 +0100

    rename selection variables

--- src/usecase1/uc1_selection.py
+++ src/usecase1/uc1_selection.py
@@ -19,34 +19,34 @@ class Uc1_First_ShortestPath(Selection):
         DES_dst = alloc_module[app_name][message.dst]
 
         #Among all possible path we choose the smallest
-        bestPath = []
-        bestDES = []
+        bestPaths = []
+        bestDESs = []
         #print (DES_dst)
 
         if message.name == self.messageToBeFiltered:
             if message.id in self.forwardedMessages:
                 message.dst = "NR_SINK" #  FORWARDAJ PORUKU NA SINK!
                 DES_dst = alloc_module[app_name]["NR_SINK"]
-                bestPath = [[node_src]]
+                bestPaths = [[node_src]]
                 for des in DES_dst: #  Nadi NR_SINK DES proces na src cvoru
                     if alloc_DES[des] == node_src:
-                        bestDES = [des]
+                        bestDESs = [des]
                         break
-                return bestPath, bestDES
+                return bestPaths, bestDESs
             self.forwardedMessages.append(message.id)
 
 
         if message.dst == message.src: # Idi u sebe! Za poruke tipa NR->NR
-            bestPath = [[node_src]]
-            bestDES = [from_des]
+            bestPaths = [[node_src]]
+            bestDESs = [from_des]
         elif message.dst == "GW":
             for des in DES_dst:
                 dst_node = alloc_DES[des]
                 # print "DES Node %i " %dst_node
                 paths = list(nx.all_simple_paths(sim.topology.G, source=node_src, target=dst_node,cutoff=1))  # Distance izmedu MM i GW je samo 1!
                 for path in paths:
-                    bestPath.append(path) # TODO: Preimenuj ovaj bestPath.... Nije uvijek samo jedan! Ovdje je za broadcast od MM-a vise njih!!
-                    bestDES.append(des)
+                    bestPaths.append(path) #  Vise pathova ce se poslati MM-u. To ce uzrokovat da dode do broadcastinga MM poruka na vise GW cvorova!
+                    bestDESs.append(des)
         else:
             best_des = dict()  # Saves DES for corresponding path.
             for des in DES_dst:
@@ -54,14 +54,14 @@ class Uc1_First_ShortestPath(Selection):
                 # print "DES Node %i " %dst_node
 
                 path = list(nx.shortest_path(sim.topology.G, source=node_src, target=dst_node))
-                bestPath.append(path)
-                bestDES.append(des)
+                bestPaths.append(path)
+                bestDESs.append(des)
                 best_des[str(path)] = des
 
             # Dohvati najkraci.
-            min(bestPath, key=len)
-            path = random.choice(bestPath) # Nekada su duljine puteva do NR cvorova iste za vise NR cvorova. Odaberi randomly koji ces. Ovdje je mogce upotrijebiti neku jos dodatnu metriku za odabir sljedeceg NR cvora ako ih je vise unutar jedne regije!
-            bestPath = [path]
-            bestDES = [best_des[str(path)]]
+            min(bestPaths, key=len)
+            path = random.choice(bestPaths) # Nekada su duljine puteva do NR cvorova iste za vise NR cvorova. Odaberi randomly koji ces. Ovdje je mogce upotrijebiti neku jos dodatnu metriku za odabir sljedeceg NR cvora ako ih je vise unutar jedne regije!
+            bestPaths = [path]
+            bestDESs = [best_des[str(path)]]
 
-        return bestPath,bestDES
+        return bestPaths,bestDESs
